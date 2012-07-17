df51d8af9acebfae99b351ef5d8ca751df8f41e4
Jun <idche@qq.com>
2012-07-17 10:45:04 +0800

    del template the first ont createFuncbody function

--- src/Jun.template.js
+++ src/Jun.template.js
@@ -13,37 +13,6 @@ Template.prototype = {
     buildFuncBody:function(){
     
         var html = ["var html = '';"];
-        var html2 = this.tmpl;
-        var openTag = this.openTag;
-        var openTagLength = openTag.length;
-        var closeTag = this.closeTag;
-        var closeTagLength = closeTag.length;
-        var index = 0;
-        
-        while( (index = html2.indexOf(openTag)) != -1  ){
-            html.push( 'html+="'+html2.slice(0, index)+'";' );
-            html2 = html2.slice(index+openTagLength);
-
-            while((index = html2.indexOf(closeTag)) != -1){
-                if(html2.slice(0, 1) == "="){
-                    html.push( 'html+= '+ html2.slice(1, index)+';');
-                }else{
-                    html.push( html2.slice(0, index)+";" );
-                }
-                
-                html2 = html2.slice(index+closeTagLength);
-                break;
-            }
-        }
-        
-        html.push('html+="'+ html2 +'";return html;');
-        return new Function("data", html.join(""));
-        
-    },
-    
-    buildFuncBody:function(){
-    
-        var html = ["var html = '';"];
         var tmpl = this.tmpl;
         var openTag = this.openTag;
         var openTagLength = openTag.length;
