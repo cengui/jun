d8c69c9ef66fea67dfd35c709fa46ae149569c56
Jun <idche@qq.com>
2012-07-17 17:17:18 +0800

    Revert "大更新"
    This reverts commit 2348235a29822ea04b1e1e606388ce47b698c9ed.

---
+++ moxian_plug-in/js/moxian_user_api.js
@@ -0,0 +1,16 @@
+﻿function BaseUserModel(){
+	this.url = "http://moxian.com/";
+};
+BaseUserModel.prototype = new BaseModel();
+Common.mix(BaseUserModel.prototype, {
+	getPath:function(url){
+		return this.url+url;
+	},
+	login:function(email, pwd){
+		Common.post(
+			this.getPath("login/gatway.php"), 
+			{"__userlable__":email,"__pwd__":pwd,"keeplogin":"off","u":"","code":""}, 
+			this.ajaxResultFunction(arguments)
+		);
+	}
+});
