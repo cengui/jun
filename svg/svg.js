05fdcc10e46b3bac137cfb642b443bc50f746f31
Jun <idche@qq.com>
2013-01-18 18:02:14 +0800

    add svg test

---
+++ svg/svg.js
@@ -0,0 +1,9 @@
+$(function(){
+	function updateSVGSize(){
+		$("#svg").attr('width', $(window).width());
+		$("#svg").attr('height', $(window).height() - 10);
+	}
+	updateSVGSize();
+	
+	$(window).resize(updateSVGSize);
+});
